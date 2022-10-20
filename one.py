210bccc69eb398c0cab1534e8f22149c185085ff
kirubhaharini <59768501+kirubhaharini@users.noreply.github.com>
2022-10-20 17:17:26 +0800

    Update one.py

--- one.py
+++ one.py
@@ -8,68 +8,19 @@ page 1 - Login
 '''
 
 def one(state):
-    with open('config.yaml') as file:
-        config = yaml.full_load(file)
-    
-    #passwords = ['userA','userB']
-    #hashed_passwords = stauth.Hasher(passwords).generate()
-    authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-    )
-
-    name, authentication_status, username = authenticator.login('Login', 'main')
-
-    # if authentication_status:
-    #     #authenticator.logout('Logout', 'main')
-    #     st.write(f'Welcome *{name}*')
-    #     st.title('Some content')
-    # elif authentication_status == False:
-    #     st.error('Username/password is incorrect')
-    # elif authentication_status == None:
-    #     st.warning('Please enter your username and password')
-
-    # if st.session_state["authentication_status"]:
-    #     authenticator.logout('Logout', 'main')
-    #     st.write(f'Welcome *{st.session_state["name"]}*')
-    #     st.title('Some content')
-    #     if st.session_state['name'] == 'userA':
-    #         st.title('user A logging in')
-    #         # NDA.NDA()
-    #     elif st.session_state['name'] == 'userB':
-    #         st.title('user B logging in')
-    #         # BSA.BSA()
-    # elif st.session_state["authentication_status"] == False:
-    #     st.error('Username/password is incorrect')
-    # elif st.session_state["authentication_status"] == None:
-    #     st.warning('Please enter your username and password')
+    names = ['groupA','groupB']
+    usernames = ['groupA','groupB']
+    passwords = ['groupA','groupB']
+    hashed_passwords = stauth.hasher(passwords).generate()
+    authenticator = stauth.authenticate(names,usernames,hashed_passwords,
+        'some_cookie_name','some_signature_key',cookie_expiry_days=30)
+    name, authentication_status = authenticator.login('Login','main')
 
 
-'''
+
     if authentication_status:
-    # st.write('Logged in as *%s*' % (name))
-        st.title('welcome')
+       # st.write('Logged in as *%s*' % (name))
+        #st.title('Some content')
+        pass
     elif authentication_status == False:
         st.error('Username/password is incorrect')
-    #elif authentication_status == None:
-    #   st.warning('Please enter your username and password')
-
-
-    if st.session_state['authentication_status']:
-    # st.write('Welcome *%s*' % (st.session_state['name']))
-        if st.session_state['name'] == 'userA':
-            st.title('user A logging in')
-            # NDA.NDA()
-        elif st.session_state['name'] == 'userB':
-            st.title('user B logging in')
-            # BSA.BSA()
-    elif st.session_state['authentication_status'] == False:
-        st.error('Username/password is incorrect')
-    elif st.session_state['authentication_status'] == None:
-        st.warning('Please enter your username and password')
-
-
-'''
